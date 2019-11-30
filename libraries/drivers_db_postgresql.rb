ee0cefa7951b8241485f9be4db2009ea6f6e5365
Ali Bahsisoglu <ali@bahsisoglu.com>
2019-11-30 20:56:37 +0300

    postgresql

--- libraries/drivers_db_postgresql.rb
+++ libraries/drivers_db_postgresql.rb
@@ -5,7 +5,12 @@ module Drivers
     class Postgresql < Base
       adapter :postgresql
       allowed_engines :postgres, :postgresql, :'aurora-postgresql'
-      packages debian: 'libpq-dev', rhel: 'postgresql96-devel'
+      packages debian: 'libpq-dev', rhel: 'postgresql-devel'
+
+      def setup
+        execute 'amazon-linux-extras install postgresql10' if node['platform'] == 'amazon'
+        super
+      end
     end
   end
 end
